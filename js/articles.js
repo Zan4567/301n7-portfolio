95ba8fcd51ab34fc1fe0f6d45599e62595f1e138
Kevin Robinson <kevinr.zan@gmail.com>
2017-05-02 17:50:53 -0700

    changing pages

--- js/articles.js
+++ js/articles.js
@@ -11,7 +11,7 @@ var articleData = [
 
   {
     name:'Introduction',
-    category:'info',
+    category:'home',
     image:'',
     description:'An introduction to the site',
     body:'<p>Welcome to my portfolio! My name is Kevin Robinson, and I am a programmer currently studying at Code Fellows.</p>'
@@ -19,7 +19,7 @@ var articleData = [
 
   {
     name:'About Me',
-    category:'info',
+    category:'about me',
     image:'',
     description:'',
     body:'<p>My name is Kevin Robinson. I live on the Eastside in Bellevue. I volunteer at the Seattle Humane Society and sometimes take care of foster cats.</p> <ul><li><a href="https://github.com/Zan4567">Github</a></li><li><a href="https://www.linkedin.com/in/kevin-robinson-13896011b/">LinkedIn</a></li></ul>'
@@ -27,9 +27,33 @@ var articleData = [
 
   {
     name:'Lorem Ipsum',
-    category:'example',
+    category:'lorem',
     image:'',
     description:'',
     body:'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+  },
+
+  {
+    name:'fsnstn',
+    category:'rbgelr',
+    image:'',
+    description:'',
+    body:''
+  },
+
+  {
+    name:'tsjhtrj',
+    category:'trhas',
+    image:'',
+    description:'',
+    body:''
+  },
+
+  {
+    name:'srjtjtgj',
+    category:'agr',
+    image:'',
+    description:'',
+    body:''
   }
 ]
--- js/project.js
+++ js/project.js
@@ -2,18 +2,74 @@
 
 var articleList = {
   array: [],
+  cats: [],
   loadArticles: function() {
     var $articleSpace = $('#articleSpace');
     var $contents = $('#toc');
+    var $navItems = $('#navItems');
+
+    //set on-click event for left menu
+    $navItems.on('click', 'li', function(event) {
+      console.log(event.target.getAttribute('data-cat'));
+      articleList.changePage(event.target.getAttribute('data-cat'));
+    });
+
     articleData.forEach(function(data) {
-      articleList.array.push(new Article(data));
+      var newArt = new Article(data);
+      if(articleList.cats.indexOf(newArt.category) < 0) {
+        articleList.cats.push(newArt.category);
+      }
+      articleList.array.push(newArt);
+    });
+    articleList.cats.forEach(function(cat) {
+      $navItems.append('<li data-cat="' + cat + '">' + cat + '</li>');
     });
     this.array.forEach(function(art) {
-      console.log(art);
-      $contents.append('<li><button class="lightButton">' + art.name + '</button></li>');
+      $contents.append('<li><button data-type="itemButton" data-name="' + art.name + '" data-cat="' + art.category + '" class="lightButton">' + art.name + '</button></li>');
       $articleSpace.append(art.toHtml());
     });
-    console.log();
+    $('button[data-type="itemButton"]').on('click', function() {
+      articleList.addArticle(this.getAttribute("data-name"));
+    });
+    articleList.changePage('home');
+  },
+  addArticle: function(name) {
+    var $articleSpace = $('#articleSpace');
+    var article;
+
+    //cancel add if the article is already there
+    if($('article[data-name="' + name + '"]').length) {
+      // console.log('attempted to add article ' + name + ' which already existed.');
+      return;
+    }
+
+    //find and add the article from articleList.array
+    for (var i = 0; i < articleList.array.length; i++) {
+      if(articleList.array[i].name === name){
+        article = articleList.array[i];
+        $articleSpace.append(article.toHtml());
+        break;
+      }
+    }
+    if(article === undefined) {
+      console.log('failed to find article ' + name);
+      return;
+    }
+  },
+  //hide navigation items that don't match current pseudo-page
+  changePage: function(page) {
+    $('#toc button').each(function() {
+      if($(this).attr('data-cat') === page) {
+        $(this).removeClass('hidden');
+      }
+      else {
+        $(this).addClass('hidden');
+      }
+    });
+
+    //close all articles, then open all articles on this page.
+    $('button.close').click();
+    $('#toc button').not('.hidden').click();
   }
 }
 
@@ -29,30 +85,42 @@ function Article(obj) {
   this.displayed = true;
 }
 
-Article.prototype.display = function () {
-  //display to the page
-  this.displayed = true;
-};
+// Article.prototype.display = function(dis) {
+//   //display to the page
+//   this.displayed = dis;
+// };
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
 
   $newArticle.removeClass('template');
-  $newArticle.find('article').attr('data-category', this.category);
-  $newArticle.find('article').attr('data-name', this.name);
+  $newArticle.attr('data-name', this.name);
+  $newArticle.attr('data-category', this.category);
   $newArticle.find('h2').html(this.name);
   $newArticle.find('.description').html(this.description);
   $newArticle.find('.body').html(this.body);
+  $newArticle.on('click', 'button.close', function() {
+    // console.log('close clicked on ' + this);
+    // console.log(this.parentNode);
+    this.parentNode.remove();
+  });
 
-  console.log($newArticle);
+  // console.log($newArticle);
   return $newArticle;
-}
+};
+
+/**
+ * Removes this article from the page.
+ */
+Article.prototype.removeThis = function() {
+  $('article[data-name="' + this.name + '"]').remove();
+};
 
 /**
  * Object for the nav bar on the left of the page.
  */
-var nav = {
-  items: [], //array of nav items.
+var leftNav = {
+  items: [], //array of nav items (strings)
   display: false, //should be displayed even if screen width is low.
   element: document.getElementById('navMenu'),
   // toggles whether the nav element should be displayed in a small-screen view.
@@ -64,14 +132,14 @@ var nav = {
       this.element.className = '';
       this.display = true;
     }
+  },
+  addNav: function(name) {
+    if(this.items.indexOf(name) >= 0) return; //prevent duplicates
+    this.items.push(name);
+    element.append('<li>' + name + '</li>');
   }
 }
 
-function NavItem(name, link) {
-  this.name = name;
-  this.link = link;
-}
-
 document.getElementById('toggleDisplay').addEventListener('click', function() {
-  nav.toggleDisplay(); //wrapper to fix 'this'
+  leftNav.toggleDisplay(); //wrapper to fix 'this'
 });
