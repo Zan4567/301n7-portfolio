6f738c390e06681f272d223227815d51ea9a6329
Kevin Robinson <kevinr.zan@gmail.com>
2017-05-09 18:23:35 -0700

    reworked to include handlebars

--- js/articles.js
+++ js/articles.js
@@ -14,7 +14,7 @@ var articleData = [
     category:'home',
     image:'',
     description:'An introduction to the site',
-    body:'<p>Welcome to my portfolio! My name is Kevin Robinson, and I am a programmer currently studying at Code Fellows in Seattle.</p>'
+    body:'Welcome to my portfolio! My name is Kevin Robinson, and I am a programmer currently studying at Code Fellows in Seattle.'
   },
 
   {
@@ -22,7 +22,7 @@ var articleData = [
     category:'about me',
     image:'',
     description:'',
-    body:'<p>My name is Kevin Robinson. I live in Bellevue, Washington. I volunteer at the Seattle Humane Society and sometimes take care of foster cats.</p> <ul><li><a href="https://github.com/Zan4567">Github</a></li><li><a href="https://www.linkedin.com/in/kevin-robinson-13896011b/">LinkedIn</a></li></ul>'
+    body:'My name is Kevin Robinson. I live in Bellevue, Washington. I volunteer at the Seattle Humane Society and sometimes take care of foster cats. <ul><li><a href="https://github.com/Zan4567">Github</a></li><li><a href="https://www.linkedin.com/in/kevin-robinson-13896011b/">LinkedIn</a></li></ul>'
   },
 
   {
@@ -30,7 +30,7 @@ var articleData = [
     category:'lorem',
     image:'',
     description:'',
-    body:'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+    body:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
 
   {
